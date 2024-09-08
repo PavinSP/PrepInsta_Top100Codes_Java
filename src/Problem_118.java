11baee0cfd2d13d383835ca9b18a6d3e394068d7
PavinSP <pavinsp122002@gmail.com>
2024-09-08 10:17:08 +0530

    Commit

---
+++ src/Problem_118.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Problem_118 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int d = scanner.nextInt();
+        System.out.println((int) (d * d * 3.14));
+    }
+}
